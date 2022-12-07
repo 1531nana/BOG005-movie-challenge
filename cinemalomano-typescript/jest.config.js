cad06f1cc49496ec780144bb051d33303e652073
Adriana Giraldo <1531nana@gmail.com>
2022-12-06 19:23:40 -0500

    unsuccessful test configurations and modifying extension of some files

--- cinemalomano-typescript/jest.config.js
+++ cinemalomano-typescript/jest.config.js
@@ -1,11 +1,18 @@
 module.exports = {
+    preset: 'ts-jest',
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.{js,jsx, tsx}'],
     coverageDirectory: 'coverage',
     testEnvironment: 'jsdom',
-      moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     moduleNameMapper: {
         "^.+\\.(css|less|scss)$": "babel-jest",
         "\\.(jpg|jpeg|png|svg)$": "babel-jest"
-      }
+      },
+
+
+      transform: {
+        '^.+\\.ts?$': 'ts-jest',
+      },
+      transformIgnorePatterns: ['<rootDir>/node_modules/'],
 }
