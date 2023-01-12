5f6d3fbce930a2e391c20c184b8cc10b4896fbff
Adriana Giraldo <1531nana@gmail.com>
2023-01-12 13:08:24 -0500

    I modified the test of the App component with more coverage

--- cinemalomano-typescript/src/App.test.tsx
+++ cinemalomano-typescript/src/App.test.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import App from './App';
 
 jest.mock('./lib/request.ts')
 
-test('renders learn react link', () => {
-  
-  // Object.defineProperty(window, 'matchMedia', {
-  //   writable: true,
-  //   value: jest.fn().mockImplementation(query => ({
-  //     matches: false,
-  //     media: query,
-  //     onchange: null,
-  //     addListener: jest.fn(), 
-  //     removeListener: jest.fn(), 
-  //     addEventListener: jest.fn(),
-  //     removeEventListener: jest.fn(),
-  //     dispatchEvent: jest.fn(),
-  //   })),
-  // });
 
-  // render(<App />);
-    // const btnLogin = screen.getByRole("button", { name: "details" });
-    // expect(btnLogin).toBeInTheDocument();
+test('The App component renders its first load in the root index', async () => {
+  
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), 
+      removeListener: jest.fn(), 
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+
+  render(<App />);
+
+  await waitFor(() => 
+  expect(window.location.pathname).toEqual('/')
+  )
   });
